b329b79d025b7eeee095bee2eba1f3eb81edf914
bc1998 <2012728@qq.com>
2018-07-15 20:16:52 +0800

    加了点东西

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,15 +4,11 @@ namespace app\ index\ controller;
 use think\ Controller;
 
 class Index extends Controller {
+
 	public
 
 	function index() {
+		$this->assign( 'tb', 'TianBing' );
 		return $this->fetch();
-	}
-
-	public
-
-	function hello( $name = 'ThinkPHP5' ) {
-		return 'hello,' . $name;
 	}
 }
